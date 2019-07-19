a3c6b4721b158e958f72bc4788ef3e5a30b507f8
Aly Kintner <aly@Alys-MacBook-Pro-2.local>
2019-07-18 23:20:01 -0400

    fixed guestbook to open new page in same tab

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ function submitGuestbook () {
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
     sessionStorage.setItem('guestbook', JSON.stringify( {name: name, address: address, email: email, message: message }));
-    window.open("gbsubmission.html", "dummy");
+    window.open("gbsubmission.html", "_self");
 }
 
