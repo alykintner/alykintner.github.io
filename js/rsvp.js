a04ee9c64ba785435df1fcf6440e938338529b5b
Aly Kintner <aly@Alys-MacBook-Pro-2.local>
2019-07-21 22:24:20 -0400

    added javascript functionality on the the rsvp page

---
+++ js/rsvp.js
@@ -0,0 +1,6 @@
+function submitRSVP() {
+    let container = document.getElementById("form-container");
+    container.style = "display: none";
+    let thankYouDiv = document.getElementById("thank-you-text");
+    thankYouDiv.style = "display: block"
+}
